6e53b8cc041d516bcbe6c21a9f657247db692104
borisRobson <brobsonc@yahoo.co.uk>
2017-05-18 09:19:20 +0100

    Update Constants.cs

--- MQTTPCL/MQTTPCL/Constants.cs
+++ MQTTPCL/MQTTPCL/Constants.cs
@@ -8,10 +8,10 @@ namespace MQTTPCL
 {
     public static class Constants
     {
-        public static string brokerAddr = "m20.cloudmqtt.com";
+        public static string brokerAddr = "";
         public static int brokerPort = 18022;
         public static string clientID = Guid.NewGuid().ToString();
-        public static string user = "ihygepmh";
-        public static string passwd = "GAB35__1D1zg";
+        public static string user = "";
+        public static string passwd = "";
     }
 }
